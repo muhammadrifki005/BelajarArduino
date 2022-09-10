e68b4e784e0266113d4fea3f15a611667dd196e8
Muhammad Rifki <112795137+muhammadrifki005@users.noreply.github.com>
2022-09-10 17:37:23 +0700

    Muhammad Rifki
    XI TKJ 1

---
+++ BelajarLED.ino
@@ -0,0 +1,93 @@
+int pinLed = 13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinLed, OUTPUT); // Menyiapkan LED siap bekerja
+  Serial.println("Program siap dimulai");
+}
+
+void loop() {
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati terlebih dahulu");
+  delay(2000); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED di jeda");
+  delay(500); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, LOW);
+  Serial.println("LED mati");
+  delay(50); // 1000 = 1 detik (jeda)
+  digitalWrite(pinLed, HIGH);
+  Serial.println("LED menyala");
+  delay(50); // 1000 = 1 detik (jeda)
+}
