a54a6e45f8b6006fbcdd0514092ecfb0f073f730
Muhammad Rifki <112795137+muhammadrifki005@users.noreply.github.com>
2022-09-17 15:46:09 +0700

    Add files via upload

---
+++ BelajarLampuLaluLintas.ino
@@ -0,0 +1,351 @@
+int LED1 = 13;
+int LED2 = 12;
+int LED3 = 11;
+int LED4 = 10;
+int LED5 = 9;
+int LED6 = 8;
+int LED7 = 7;
+int LED8 = 6;
+int LED9 = 5;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+  pinMode(LED7, OUTPUT);
+  pinMode(LED8, OUTPUT);
+  pinMode(LED9, OUTPUT);
+  Serial.println("Program siap dimulai");
+}
+
+void loop() {
+
+  // Lampu menyala dua kali
+  
+  digitalWrite(LED1, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED1, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED1, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED1, LOW);
+  Serial.println("LED mati");
+  delay(300);
+  
+  digitalWrite(LED2, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED2, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED2, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED2, LOW);
+  Serial.println("LED mati");
+  delay(300);
+  
+  digitalWrite(LED3, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED3, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED3, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED3, LOW);
+  Serial.println("LED mati");
+  delay(300);
+  
+  digitalWrite(LED4, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED4, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED4, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED4, LOW);
+  Serial.println("LED mati");
+  delay(300);
+  
+  digitalWrite(LED5, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED5, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED5, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED5, LOW);
+  Serial.println("LED mati");
+  delay(300);
+  
+  digitalWrite(LED6, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED6, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED7, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED7, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED7, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED7, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED8, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED8, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED8, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED8, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED9, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED9, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED9, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED9, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED8, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED8, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED8, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED8, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED7, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED7, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED7, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED7, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED6, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED6, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED5, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED5, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED5, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED5, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED4, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED4, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED4, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED4, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED3, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED3, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED3, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED3, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  digitalWrite(LED2, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED2, LOW);
+  Serial.println("LED mati");
+  delay(100);
+  digitalWrite(LED2, HIGH);
+  Serial.println("LED menyala");
+  delay(100);
+  digitalWrite(LED2, LOW);
+  Serial.println("LED mati");
+  delay(300);
+
+  // Lampu menyala satu kali
+  
+  digitalWrite(LED1, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED1, LOW);
+  Serial.println("LED mati");
+  delay(50);
+  
+  digitalWrite(LED2, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED2, LOW);
+  Serial.println("LED mati");
+  delay(50);
+  
+  digitalWrite(LED3, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED3, LOW);
+  Serial.println("LED mati");
+  delay(50);
+  
+  digitalWrite(LED4, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED4, LOW);
+  Serial.println("LED mati");
+  delay(50);
+  
+  digitalWrite(LED5, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED5, LOW);
+  Serial.println("LED mati");
+  delay(50);
+  
+  digitalWrite(LED6, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED mati");
+  delay(50);
+   
+  digitalWrite(LED7, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED7, LOW);
+  Serial.println("LED mati");
+  delay(50);
+   
+  digitalWrite(LED8, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED8, LOW);
+  Serial.println("LED mati");
+  delay(50);
+   
+  digitalWrite(LED9, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED9, LOW);
+  Serial.println("LED mati");
+  delay(50);
+
+  digitalWrite(LED8, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED8, LOW);
+  Serial.println("LED mati");
+  delay(50);
+
+  digitalWrite(LED7, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED7, LOW);
+  Serial.println("LED mati");
+  delay(50);
+
+  digitalWrite(LED6, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED mati");
+  delay(50);
+
+  digitalWrite(LED5, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED5, LOW);
+  Serial.println("LED mati");
+  delay(50);
+
+  digitalWrite(LED4, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED4, LOW);
+  Serial.println("LED mati");
+  delay(50);
+
+  digitalWrite(LED3, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED3, LOW);
+  Serial.println("LED mati");
+  delay(50);
+
+  digitalWrite(LED2, HIGH);
+  Serial.println("LED menyala");
+  delay(50);
+  digitalWrite(LED2, LOW);
+  Serial.println("LED mati");
+  delay(100);
+}
